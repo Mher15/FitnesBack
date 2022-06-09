ac284b972bba845c43470bddff0d1df84ca7eb43
Hayrudz <bedros.dev@gmail.com>
2022-06-09 21:51:36 +0400

    payment

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -1,21 +1,29 @@
 
-import Stripe from "stripe"
+// import Stripe from "stripe"
 
-export class PaymentController {
-  static index (req, res) {
-    let status, error;
-    const {token, amount} = req.body;
-   try {
-      Stripe.charfes.create({
-       source: token.id,
-       amount,
-       currency: 'usd',
-     });
-     status = 'success';
-   } catch (error){
-     console.log(error);
-     status = 'Failure';
-   }
-   res.json({ error, status })
-  }
-}
+// export class PaymentController {
+//   static async index (req, res) {
+// 	await req
+// 	let { amount, id } = req.body
+//    try {
+//      const payment = stripe.paymentIntents.create({
+// 			amount: 500,
+// 			currency: "USD",
+// 			description: "Spatula company",
+// 			payment_method: id,
+// 			confirm: true
+// 		})
+// 		console.log("Payment", payment)
+// 		res.json({
+// 			message: "Payment successful",
+// 			success: true
+// 		})
+//    } catch (error){
+//     console.log("Error", error)
+// 		res.json({
+// 			message: "Payment failed",
+// 			success: false
+// 		})
+//    }
+//   }
+// }
