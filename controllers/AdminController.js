b37ae518d35a52e055be41207788ac98a2587212
Mher15 <mher.janikyan01@gmail.com>
2022-06-15 20:13:09 +0400

    create new changes

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,5 +1,29 @@
+import curs from "../model/curs.js";
+
 export class AdminController {
   static index(req, res) {
     res.send("hello");
+  }
+  static update(req, res) {
+    console.log(req.body)
+    // curs
+      // .findOneAndUpdate(
+      //   { _id: "62a5e1de0eafd7fee73eb0a1" },
+      //   { $set: { title: "tessssss" } },
+      //   { new: true }
+      // )
+      // .then((item) => {
+      //   console.log(item);
+      // });
+      // res.end()
+  }
+  static delete(req, res) {
+    curs.deleteOne({ _id: req.body.id })
+    .then((res)=>{
+      console.log(res)
+    })
+    .catch(error=>{
+      console.log(error)
+    })
   }
 }
--- routes/AdminRouts.js
+++ routes/AdminRouts.js
@@ -4,5 +4,7 @@ const route = express.Router();
 import { AdminController } from "../controllers/AdminController.js";
 
 route.get("/", AdminController.index);
+route.post("/update", AdminController.update);
+route.post("/delete", AdminController.delete);
 
 export default route;
