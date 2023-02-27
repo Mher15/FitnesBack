e76d16d923649ea9e8ef68505e140630126b56da
JamalyanK <jamalyank96@gmail.com>
2023-02-28 00:12:45 +0400

    asdasd

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "/build")));
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
